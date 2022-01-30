bc1aa790e9725fdce1d8ba34c33b43e18a71afe6
Will Shepp <28759252+willshepp28@users.noreply.github.com>
2022-01-30 11:45:52 -0500

    created a parent module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParentModule } from './parent/parent.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
+    ParentModule,
   ],
   controllers: [AppController],
   providers: [AppService],
